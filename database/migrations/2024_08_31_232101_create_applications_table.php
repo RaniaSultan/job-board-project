4b30443458334107d4d71a2d16e32d5c4fb8c079
mona-abdelmohsen <monaabdelmohsen101214@gmail.com>
2024-09-06 01:03:42 +0300

    last update the migration files

--- database/migrations/2024_08_31_232101_create_applications_table.php
+++ database/migrations/2024_08_31_232101_create_applications_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
             $table->string('resume')->nullable();
+            $table->enum('status', ['waiting', 'accepted', 'rejected', 'cancelled'])->default('waiting');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
             $table->primary(['user_id', 'post_id']);
