991239c4a7bef8185ae9f9f4f0e253c6c13a8ba2
sandymosaad <sandyzakher5@gmail.com>
2024-09-09 13:46:40 +0200

    views update

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -6,44 +6,44 @@ All Applications
 
 @section('navbar')
 <li class="navbar-item"><a class="nav-link" href="{{ route('applications.index', ['status' => 'waiting']) }}">Pending
-    Applications</a></li>
+        Applications</a></li>
 <li class="navbar-item"><a class="nav-link" href="{{ route('applications.index', ['status' => 'accepted']) }}">Accepted
-    Applications</a></li>
+        Applications</a></li>
 <li class="navbar-item"><a class="nav-link" href="{{ route('applications.index', ['status' => 'rejected']) }}">Rejected
-    Applications</a></li>
+        Applications</a></li>
 <li class="navbar-item"><a class="nav-link"
-    href="{{ route('applications.index', ['status' => 'cancelled']) }}">Cancelled Applications</a></li>
+        href="{{ route('applications.index', ['status' => 'cancelled']) }}">Cancelled Applications</a></li>
 @endsection
 
 @section('main')
 @if (session('status'))
-  <div class="alert alert-success mx-auto my-2 w-75">
+<div class="alert alert-success mx-auto my-2 w-75">
     {{ session('status') }}
-  </div>
+</div>
 @endif
 
 @foreach ($applications as $application)
-  <div class="card mx-auto my-2 w-75">
+<div class="card mx-auto my-2 w-75">
     <div class="card-header">
-    {{$application->user->name}}
+        {{$application->user->name}}
     </div>
     <div class="card-body">
-    <h5 class="card-title">Applicant jobTitle</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="{{ route('applications.show', $application) }}" class="btn btn-primary">Show Details</a>
+        <h5 class="card-title">Applicant jobTitle</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="{{ route('applications.show', $application) }}" class="btn btn-primary">Show Details</a>
 
-    @if ($currentStatus == 'waiting')
-    <form action="{{ route('applications.accept', $application) }}" method="POST" style="display:inline;">
-      @csrf
-      <button type="submit" class="btn btn-success">Accept</button>
-    </form>
-    <form action="{{ route('applications.reject', $application) }}" method="POST" style="display:inline;">
-      @csrf
-      <button type="submit" class="btn btn-danger">Reject</button>
-    </form>
-  @endif
+        @if ($currentStatus == 'waiting')
+        <form action="{{ route('applications.accept', $application) }}" method="POST" style="display:inline;">
+            @csrf
+            <button type="submit" class="btn btn-success">Accept</button>
+        </form>
+        <form action="{{ route('applications.reject', $application) }}" method="POST" style="display:inline;">
+            @csrf
+            <button type="submit" class="btn btn-danger">Reject</button>
+        </form>
+        @endif
     </div>
-  </div>
+</div>
 @endforeach
 {{ $applications->links() }}
 @endsection
